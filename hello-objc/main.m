b825667815adbc6384e3e82d09e64a933b072bcc
dj-viking <anders.swedishviking@gmail.com>
2022-09-03 11:29:37 -0400

    Initial Commit

---
+++ hello-objc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hello-objc
+//
+//  Created by Anders Ackerman on 9/3/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
